15880b323691bb7584b6c8cae43470bf5af366d6
Kiritgoti <kiritgoti007@gmail.com>
2022-04-07 15:19:08 +0530

    Kirit uodate 07

--- asset/js/custome.js
+++ asset/js/custome.js
@@ -6,3 +6,20 @@
 //     };
 //     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 //     }
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
